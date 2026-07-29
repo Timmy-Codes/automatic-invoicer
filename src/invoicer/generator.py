6f320f2f216304093ec654aed24b320ddb8ab015
Tim <timglasgow80@gmail.com>
2026-07-29 21:13:42 +1000

    feat: load Word template

--- src/invoicer/generator.py
+++ src/invoicer/generator.py
@@ -1,2 +1,9 @@
+from docx import Document
+
 class InvoiceGenerator:
-    pass
+    def __init__(self, template_path):
+        self.template_path = template_path
+        self.document = None
+
+    def load_template(self):
+        self.document = Document(self.template_path)
